55c10c28be25bcb87ad4e40f4410c31ac3a543db
Dominic <dominicvana@gmail.com>
2022-04-16 21:04:43 -0500

    Chore: remove dist dir from repo

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
 
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'SpaceX Rockets',
+    }),
+  ],
+
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 
   module: {
