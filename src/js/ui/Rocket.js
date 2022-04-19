fa8b7b99fa626f9ea7b2ca657908299c2b51cf38
Dominic <dominicvana@gmail.com>
2022-04-19 17:53:05 -0500

    Feat: init mobile styling

--- src/js/ui/Rocket.js
+++ src/js/ui/Rocket.js
@@ -25,37 +25,39 @@ export default function Rocket({
 }) {
   const html = `
   <h2>${name}</h2>
-  <img src=${images[0]}>
-  <ul>
-    <li>Status: ${active ? 'active' : 'inactive'}</li>
-    <li>Stages: ${stages}</li>
-    <li>Boosters: ${boosters}</li>
-    <li>Success rate: ${successRatePct}%</li>
-    <li>Cost per launch: ${costPerLaunch}</li>
-    <li>First flight: ${firstFlight}, ${country}</li>
-    <li>Height: ${height.meters}m</li>
-    <li>Mass: ${mass.kg}kg</li>
-    <li>First stage</li>
-    <ul>
-      <li>Engines: ${firstStageEngines}</li>
-      <li>Fuel amount: ${firstStageFuelAmountTons} tons</li>
-      <li>Burn time: ${firstStageBurnTimeSec} seconds</li>
-      <li>${
-        firstStageReusable ? 'Reusable' : '<strong>Not</strong> reusable'
-      }</li>
+
+  <div class="background" style="background-image: url(${images[0]})">
+    <ul class="rocket-info">
+      <li>Status: ${active ? 'active' : 'inactive'}</li>
+      <li>Stages: ${stages}</li>
+      <li>Boosters: ${boosters}</li>
+      <li>Success rate: ${successRatePct}%</li>
+      <li>Cost per launch: ${costPerLaunch}</li>
+      <li>First flight: ${firstFlight}, ${country}</li>
+      <li>Height: ${height.meters}m</li>
+      <li>Mass: ${mass.kg}kg</li>
+      <li>First stage</li>
+      <ul>
+        <li>Engines: ${firstStageEngines}</li>
+        <li>Fuel amount: ${firstStageFuelAmountTons} tons</li>
+        <li>Burn time: ${firstStageBurnTimeSec} seconds</li>
+        <li>${
+          firstStageReusable ? 'Reusable' : '<strong>Not</strong> reusable'
+        }</li>
+      </ul>
+      <li>Second stage</li>
+      <ul>
+        <li>Engines: ${secondStageEngines}</li>
+        <li>Fuel amount: ${secondStageFuelAmountTons} tons</li>
+        <li>Burn time: ${secondStageBurnTimeSec} seconds</li>
+        <li>${
+          secondStageReusable ? 'Reusable' : '<strong>Not</strong> reusable'
+        }</li>
+      </ul>
+      <li>Description: ${description}</li>
+      <li>Read more on <a href="${wikipedia}" target="_blank">wikipedia</a> </li>
     </ul>
-    <li>Second stage</li>
-    <ul>
-      <li>Engines: ${secondStageEngines}</li>
-      <li>Fuel amount: ${secondStageFuelAmountTons} tons</li>
-      <li>Burn time: ${secondStageBurnTimeSec} seconds</li>
-      <li>${
-        secondStageReusable ? 'Reusable' : '<strong>Not</strong> reusable'
-      }</li>
-    </ul>
-    <li>Description: ${description}</li>
-    <li>Read more on <a href="${wikipedia}" target="_blank">wikipedia</a> </li>
-  </ul>
+  </div>
   `;
 
   const parentElement = Component('section', { class: 'rocket' }, html);
