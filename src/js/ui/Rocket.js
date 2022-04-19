33707f1125a89202a353a3bf84d8b8cbd15abfb9
Dominic <dominicvana@gmail.com>
2022-04-19 17:24:46 -0500

    Feat: Rocket component html populated with data

--- src/js/ui/Rocket.js
+++ src/js/ui/Rocket.js
@@ -29,7 +29,32 @@ export default function Rocket({
   <ul>
     <li>Status: ${active ? 'active' : 'inactive'}</li>
     <li>Stages: ${stages}</li>
+    <li>Boosters: ${boosters}</li>
+    <li>Success rate: ${successRatePct}%</li>
     <li>Cost per launch: ${costPerLaunch}</li>
+    <li>First flight: ${firstFlight}, ${country}</li>
+    <li>Height: ${height.meters}m</li>
+    <li>Mass: ${mass.kg}kg</li>
+    <li>First stage</li>
+    <ul>
+      <li>Engines: ${firstStageEngines}</li>
+      <li>Fuel amount: ${firstStageFuelAmountTons} tons</li>
+      <li>Burn time: ${firstStageBurnTimeSec} seconds</li>
+      <li>${
+        firstStageReusable ? 'Reusable' : '<strong>Not</strong> reusable'
+      }</li>
+    </ul>
+    <li>Second stage</li>
+    <ul>
+      <li>Engines: ${secondStageEngines}</li>
+      <li>Fuel amount: ${secondStageFuelAmountTons} tons</li>
+      <li>Burn time: ${secondStageBurnTimeSec} seconds</li>
+      <li>${
+        secondStageReusable ? 'Reusable' : '<strong>Not</strong> reusable'
+      }</li>
+    </ul>
+    <li>Description: ${description}</li>
+    <li>Read more on <a href="${wikipedia}" target="_blank">wikipedia</a> </li>
   </ul>
   `;
 
